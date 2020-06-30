5c9801934de2e70d08d1a9ece5112cf22fd3c5b7
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-06-29 21:46:34 -0700

    Leave out name finder for now

---
+++ getnames.py
@@ -0,0 +1,12 @@
+import urllib.request
+import json
+
+
+# def printNames(num, gender):
+#     data = urllib.request.urlopen(
+#         f"http://api.toys/api/medieval_name?num=1")
+#     nData = json.loads(data.read())
+#     return f"{nData}"
+
+
+# print(printNames("2", "male"))
